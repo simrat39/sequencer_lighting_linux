27979a74b74c7b545cb6667a28c4888cd003258b
simrat39 <simrats169169@gmail.com>
2021-03-25 17:19:37 -0700

    Always select the corretc hid device

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,21 @@ use omen_rgb_test::multizone::{
 
 fn main() {
     let hid = hidapi::HidApi::new().unwrap();
-    for device in hid.device_list().skip(2) {
+    let mut omen_devices = Vec::new();
+
+    for device in hid.device_list() {
         if device.vendor_id() == 1008 && device.product_id() == 8001 {
-            let dev = &device.open_device(&hid).unwrap();
-            let a = MidShimmer {
-                fg_color: Color::from(0, 0xff, 0xff),
-                device: dev,
-                bg_color: Color::from(0xff, 0, 0),
-            };
-            a.start::<FullStatic>();
+            omen_devices.push(device);
         }
     }
+
+    let last_omen_device = omen_devices.iter().last().unwrap();
+    let dev = last_omen_device.open_device(&hid).unwrap();
+
+    let a = MidShimmer {
+        fg_color: Color::from(255, 0, 255),
+        bg_color: Color::from(0, 255, 0),
+        device: &dev,
+    };
+    a.start::<FullStatic>();
 }
